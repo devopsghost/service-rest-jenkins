b3e15211ecfe8c13cb1e1ea361a7f142f7e1d30f
ghostnsnow <ghostnsnow@users.noreply.github.com>
2024-12-18 21:02:49 +0530

    setup

---
+++ src/main/java/org/chintanu/service_rest_jenkins/ServiceRestJenkinsApplication.java
@@ -0,0 +1,13 @@
+package org.chintanu.service_rest_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRestJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRestJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/chintanu/service_rest_jenkins/ServiceRestJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chintanu.service_rest_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRestJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
